46a3c9e64ccd69d04a2f7ca4c669954f3485c3b9
GoldBoy1001 <chaplinsergey29@gmail.com>
2023-11-25 14:50:53 +0300

    add files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles/main.css"
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router,  Routes, Route } from "react-router-dom";
 
 import Nav from "./components/nav/Nav"
 import Footer from "./components/footer/Footer";
