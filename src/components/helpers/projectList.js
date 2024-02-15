5550774790eb3c49a0a53d20389cc380cd4c5e20
GoldBoy1001 <chaplinsergey29@gmail.com>
2024-02-15 13:53:46 +0300

    remove project

--- src/components/helpers/projectList.js
+++ src/components/helpers/projectList.js
@@ -1,12 +1,12 @@
-import project01 from '../../img/projects/project1.jpg'
-import project01Big from '../../img/projects/01-big.jpg'
-import project02 from '../../img/projects/xiaomi.jpg'
-import project02Big from '../../img/projects/02-big.jpg'
-import project03 from '../../img/projects/аукцион.jpg'
-import project03Big from '../../img/projects/03-big.jpg'
-import project04 from '../../img/projects/holst.jpg'
-import drafts from '../../img/projects/drafts.jpg'
-import prof from '../../img/projects/prof.jpg'
+import project01 from "../../img/projects/project1.jpg";
+import project01Big from "../../img/projects/01-big.jpg";
+import project02 from "../../img/projects/xiaomi.jpg";
+import project02Big from "../../img/projects/02-big.jpg";
+import project03 from "../../img/projects/аукцион.jpg";
+import project03Big from "../../img/projects/03-big.jpg";
+import project04 from "../../img/projects/holst.jpg";
+import drafts from "../../img/projects/drafts.jpg";
+import prof from "../../img/projects/prof.jpg";
 // import project04Big from '../../img/projects/04-big.jpg'
 // import project05 from '../../img/projects/05.jpg'
 // import project05Big from '../../img/projects/05-big.jpg'
@@ -14,78 +14,70 @@ import prof from '../../img/projects/prof.jpg'
 // import project06Big from '../../img/projects/06-big.jpg'
 
 const project = [
-	{
-		title: 'ProffiComplex',
-		skills: 'Gulp, JavaScript, Scss, Grid, Yandex Maps, Swiper',
-		img: prof,
-		id: 1,
-		imgBig: prof,
-		gitHubLink: 'https://goldboy1001.github.io/profficomplex/'
-	},
-	{
-		title: 'Drafts',
-		skills: 'Gulp, JavaScript, Scss, Grid',
-		img: drafts,
-		id: 2,
-		imgBig: drafts,
-		gitHubLink: 'https://goldboy1001.github.io/drafts/'
-	},
-	{
-		title: 'Booking Tickets',
-		skills: 'React, TypeScript, Redux Toolkit, Scss, Grid',
-		img: project01,
-		id: 3,
-		imgBig: project01,
-		gitHubLink: 'https://goldboy1001.github.io/fe-diplom/'
-	},
-	{
-		title: 'Holst',
-		skills: 'Gulp, JavaScript, Scss, Grid',
-		img: project04,
-		id: 4,
-		imgBig: project04,
-		gitHubLink: 'https://goldboy1001.github.io/holst/'
-	},
-	{
-		title: 'Xiaomi-Bike',
-		skills: 'Gulp, js, jquery',
-		img: project02,
-		id: 5,
-		imgBig: project02,
-		gitHubLink: 'https://goldboy1001.github.io/Xiaomi-Bike/'
-	},
-	{
-		title: 'Auction',
-		skills: 'JS, Gulp',
-		img: project03,
-		id: 6,
-		imgBig: project03,
-		gitHubLink: 'https://goldboy1001.github.io/auction/'
-	},
-	// {
-	// 	title: 'Dating app',
-	// 	skills: 'React, Node.js, MongoDB',
-	// 	img: project04,
-	// 	id: 4,
-	// 	imgBig: project04Big,
-	// 	gitHubLink: 'https://github.com'
-	// },
-	// {
-	// 	title: 'Landing',
-	// 	skills: 'React, Node.js, MongoDB',
-	// 	img: project05,
-	// 	id: 5,
-	// 	imgBig: project05Big,
-	// 	gitHubLink: 'https://github.com'
-	// },
-	// {
-	// 	title: 'Gaming community',
-	// 	skills: 'React, Node.js, MongoDB',
-	// 	img: project06,
-	// 	id: 6,
-	// 	imgBig: project06Big,
-	// 	gitHubLink: 'https://github.com'
-	// },
-]
+  {
+    title: "ProffiComplex",
+    skills: "Gulp, JavaScript, Scss, Grid, Yandex Maps, Swiper",
+    img: prof,
+    id: 1,
+    imgBig: prof,
+    gitHubLink: "https://goldboy1001.github.io/profficomplex/",
+  },
+  {
+    title: "Drafts",
+    skills: "Gulp, JavaScript, Scss, Grid",
+    img: drafts,
+    id: 2,
+    imgBig: drafts,
+    gitHubLink: "https://goldboy1001.github.io/drafts/",
+  },
+  {
+    title: "Booking Tickets",
+    skills: "React, TypeScript, Redux Toolkit, Scss, Grid",
+    img: project01,
+    id: 3,
+    imgBig: project01,
+    gitHubLink: "https://goldboy1001.github.io/fe-diplom/",
+  },
+  {
+    title: "Xiaomi-Bike",
+    skills: "Gulp, js, jquery",
+    img: project02,
+    id: 4,
+    imgBig: project02,
+    gitHubLink: "https://goldboy1001.github.io/Xiaomi-Bike/",
+  },
+  {
+    title: "Auction",
+    skills: "JS, Gulp",
+    img: project03,
+    id: 5,
+    imgBig: project03,
+    gitHubLink: "https://goldboy1001.github.io/auction/",
+  },
+  // {
+  // 	title: 'Dating app',
+  // 	skills: 'React, Node.js, MongoDB',
+  // 	img: project04,
+  // 	id: 4,
+  // 	imgBig: project04Big,
+  // 	gitHubLink: 'https://github.com'
+  // },
+  // {
+  // 	title: 'Landing',
+  // 	skills: 'React, Node.js, MongoDB',
+  // 	img: project05,
+  // 	id: 5,
+  // 	imgBig: project05Big,
+  // 	gitHubLink: 'https://github.com'
+  // },
+  // {
+  // 	title: 'Gaming community',
+  // 	skills: 'React, Node.js, MongoDB',
+  // 	img: project06,
+  // 	id: 6,
+  // 	imgBig: project06Big,
+  // 	gitHubLink: 'https://github.com'
+  // },
+];
 
-export { project }
+export { project };
